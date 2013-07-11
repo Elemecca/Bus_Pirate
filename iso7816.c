84dc2616fc17c34dd6b37599ce507ec29741672a
Sam Hanes <sam@maltera.com>
2013-07-10 18:30:43 -0700

    Attempt clock output via output compare.
    This does generate a 4 MHz square wave, but it's not in spec for
    ISO 7816-3:2006 s5.2.3. In particular, it ranges -1.2 - 3.0 V
    instead of the required (-0.3, 2.5) - (3.5, 5.3) V. The rise and
    fall times are also too long and the waveform isn't very square.
    
    Try, try again.

--- iso7816.c
+++ iso7816.c
@@ -30,11 +30,28 @@ unsigned int ISO7816read (void) {
 }
 
 void ISO7816setup (void) {
+    // set up timer 2 for clock output
+    T2CON           = 0;    // disable the timer
+    TMR2            = 0;    // clear the timer register
+    PR2             = 1;    // period of 1 at 16 MHz = 4 MHz
+    IFS0bits.T2IF   = 0;    // disable the timer interrupt
+    IEC0bits.T2IE   = 0;    // "
+    IPC1bits.T2IP   = 0;    // "
 
+    // set up output comparator 5 for clock output
+    BP_CLK_RPOUT    = OC5_IO;   // output on CLK pin
+    OC5CON          = 0x0001;   // initial low, use timer 2, prescaler 1:1
+    OC5CONbits.OCM1 = 1;        // enable toggle mode
+    OC5R            = PR2;      // OCxR = PRx toggles once per period
+    IFS2bits.OC5IF  = 0;        // disable the comparator interrupt
+    IEC2bits.OC5IE  = 0;        // "
+    IPC10bits.OC5IP = 0;        // "
 }
 
 void ISO7816cleanup (void) {
-
+    T2CON           = 0;
+    OC5CON          = 0;
+    BP_CLK_RPOUT    = 0;
 }
 
 void ISO7816macro (unsigned int c) {
@@ -42,11 +59,11 @@ void ISO7816macro (unsigned int c) {
 }
 
 void ISO7816start (void) {
-
+    T2CONbits.TON = 1;
 }
 
 void ISO7816stop (void) {
-
+    T2CONbits.TON = 0;
 }
 
 unsigned int ISO7816periodic (void) {
@@ -54,7 +71,7 @@ unsigned int ISO7816periodic (void) {
 }
 
 void ISO7816pins (void) {
-
+    bpWline( "CLK\t-\t-\t-" );
 }
 
 void ISO7816settings (void) {
